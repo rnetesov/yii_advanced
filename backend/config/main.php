c6ba5b954858d37b0cee24f12e7332d4f33b7949
Roman <rnetesov@gmail.com>
2021-06-13 01:07:46 +0500

    modified main.php

--- backend/config/main.php
+++ backend/config/main.php
@@ -14,6 +14,7 @@ return [
     'modules' => [],
     'components' => [
         'request' => [
+            'baseUrl' => '/backend',
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
